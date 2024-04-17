36a0188c1a0b7c55996e9139c865427fe537e52d
hachen1234 <90781150+hachen1234@users.noreply.github.com>
2024-04-17 09:51:02 -0400

    1

---
+++ project/src/lib/SupabaseClient.js
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js'
+export const supabase = createClient(
+  'https://zltjxgueweobufzamksv.supabase.co',
+  '<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpsdGp4Z3Vld2VvYnVmemFta3N2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4NDMzNTcsImV4cCI6MjAyODQxOTM1N30.mv-Os84_cQFKlMLRhpOT_tAdJn7EXg8-FVENdaIzr2E>'
+)
--- project/src/views/HomeView.vue
+++ project/src/views/HomeView.vue
@@ -7,12 +7,6 @@
   </div>
 </template>
 
-<script>
-import { createClient } from '@supabase/supabase-js'
-export const supabase = createClient(
-  'https://zltjxgueweobufzamksv.supabase.co',
-  '<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpsdGp4Z3Vld2VvYnVmemFta3N2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4NDMzNTcsImV4cCI6MjAyODQxOTM1N30.mv-Os84_cQFKlMLRhpOT_tAdJn7EXg8-FVENdaIzr2E>'
-)
-</script>
+<script setup></script>
 
 <style lang="scss" scoped></style>
