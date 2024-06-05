c27ee40967be9c53643c9c21e0307f08d78b9549
hachen1234 <90781150+hachen1234@users.noreply.github.com>
2024-06-05 10:15:16 -0400

    1

--- project/src/components/AuthUser.vue
+++ project/src/components/AuthUser.vue
@@ -44,7 +44,8 @@ const signup = async () => {
 
 <template>
   <form class="row flex-center flex" @submit.prevent="handleLogin">
-    <div class="col-6 form-widget">
+    <h1>login page</h1>
+    <div class="col-6-form-widget" id="col-6-form-widget">
       <div>
         <input class="inputField" required type="email" placeholder="Your email" v-model="email" />
       </div>
@@ -57,8 +58,64 @@ const signup = async () => {
           v-model="password"
         />
       </div>
-      <div><input type="submit" class="button" @click="signup" /> signup</div>
-      <div><input type="submit" class="button" @click="login" /> login</div>
+      <div><input type="submit" class="button" id="button" @click="signup" /> signup</div>
+      <div><input type="submit" class="button" id="button" @click="login" /> login</div>
     </div>
   </form>
 </template>
+<style lang="css">
+body * {
+  box-sizing: border-box;
+  font-size: 110%;
+  margin: 0;
+  padding: 0;
+}
+.col-6-form-widget {
+  width: 30rem;
+  margin: 1rem auto;
+  border-radius: 2rem;
+  object-fit: cover;
+  border-radius: 2rem;
+  justify-content: space-around;
+  margin-top: 6rem;
+}
+.button {
+  display: inline-block;
+  outline: 0;
+  cursor: pointer;
+  border: none;
+  padding: 0 5rem;
+  height: 2.5rem;
+  line-height: 2.5rem;
+  border-radius: 7px;
+  background-color: #0070f3;
+  color: white;
+  font-weight: 400;
+  font-size: 1.5rem;
+  box-shadow: 0 4px 14px 0 rgb(0 118 255 / 39%);
+  transition:
+    background 0.2s ease,
+    color 0.2s ease,
+    box-shadow 0.2s ease;
+  :hover {
+    background: rgba(0, 118, 255, 0.9);
+    box-shadow: 0 6px 20px rgb(0 118 255 / 23%);
+  }
+}
+input {
+  padding: 0.5rem, 1rem;
+  background: rgb(31, 32, 35);
+  border: 1px solid rgb(60, 63, 68);
+  border-radius: 4px;
+  font-size: 2rem;
+  color: rgb(247, 248, 248);
+  height: 3rem;
+  appearance: none;
+  transition: border 0.15s ease 0s;
+  :focus {
+    outline: none;
+    box-shadow: none;
+    border-color: rgb(100, 153, 255);
+  }
+}
+</style>
