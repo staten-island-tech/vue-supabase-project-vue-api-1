5a3adab04b045647b5e12194897d53a6daac6b7e
hachen1234 <90781150+hachen1234@users.noreply.github.com>
2024-06-04 23:26:11 -0400

    1

---
+++ project/src/views/PostView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>this is post</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
