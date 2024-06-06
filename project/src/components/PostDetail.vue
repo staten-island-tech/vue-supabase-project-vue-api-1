c221e3024ab1c7d0e18b6a0e438dfec2d45260e7
hachen1234 <90781150+hachen1234@users.noreply.github.com>
2024-06-06 10:30:10 -0400

    Update PostDetail.vue

--- project/src/components/PostDetail.vue
+++ project/src/components/PostDetail.vue
@@ -5,11 +5,15 @@ import router from '@/router'
 const go_to_main_site = async () => {
   router.push('/mainsite')
 }
+const posting = async () => {
+  const { error } = await supabase.from('everythingelse').insert({ input: 'hehe' })
+}
 </script>
 
 <style lang="scss" scoped></style>
 <template>
   <div>
     <div><input type="submit" class="button" @click="go_to_main_site" /> go back</div>
+    <div><input type="submit" class="button" @click="posting" /> post</div>
   </div>
 </template>
